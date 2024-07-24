d7ae6babde40a2f20322314cc66fc6b252485da0
Henok Mekonnen <Henokmekonnen5@gmail.com>
2024-07-24 22:10:34 +0300

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,89 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+// Custom color with css variable color in __theme_color.scss
+function customColors(cssVar: string) {
+  return ({ opacityVariable, opacityValue }: { opacityVariable?: string; opacityValue?: number }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${cssVar}), ${opacityValue})`;
+    }
+    if (opacityVariable !== undefined) {
+      return `rgba(var(${cssVar}), var(${opacityVariable}, 1))`;
+    }
+    return `rgb(var(${cssVar}))`;
+  };
+}
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        "2xl": "128px",
+      },
+    },
+    // fontFamily: {
+    //   display: ["var(--font-display)", ...defaultTheme.fontFamily.sans],
+    //   body: ["var(--font-body)", ...defaultTheme.fontFamily.sans],
+    // },
+
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-bg': "url('https://plus.unsplash.com/premium_photo-1661962660197-6c2430fb49a6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+      colors: {
+       
+        primary: {
+          10: customColors("--c-primary-10"),
+          50: customColors("--c-primary-50"),
+          100: customColors("--c-primary-100"),
+          200: customColors("--c-primary-200"),
+          300: customColors("--c-primary-300"),
+          400: customColors("--c-primary-400"),
+          500: customColors("--c-primary-500"),
+          6000: customColors("--c-primary-600"),
+          700: customColors("--c-primary-700"),
+          800: customColors("--c-primary-800"),
+          900: customColors("--c-primary-900"),
+        },
+        secondary: {
+          50: customColors("--c-secondary-50"),
+          100: customColors("--c-secondary-100"),
+          200: customColors("--c-secondary-200"),
+          300: customColors("--c-secondary-300"),
+          400: customColors("--c-secondary-400"),
+          500: customColors("--c-secondary-500"),
+          6000: customColors("--c-secondary-600"),
+          700: customColors("--c-secondary-700"),
+          800: customColors("--c-secondary-800"),
+          900: customColors("--c-secondary-900"),
+        },
+        neutral: {
+          50: customColors("--c-neutral-50"),
+          100: customColors("--c-neutral-100"),
+          200: customColors("--c-neutral-200"),
+          300: customColors("--c-neutral-300"),
+          400: customColors("--c-neutral-400"),
+          500: customColors("--c-neutral-500"),
+          6000: customColors("--c-neutral-600"),
+          700: customColors("--c-neutral-700"),
+          800: customColors("--c-neutral-800"),
+          900: customColors("--c-neutral-900"),
+        },
+        'custom-blue': '#17416a',
       },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
-export default config;
