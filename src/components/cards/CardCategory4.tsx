aded224b598d251ba6a597a6bc2a6f23407882ce
Henok Mekonnen <Henokmekonnen5@gmail.com>
2024-07-25 08:44:32 +0300

    category four

--- src/components/cards/CardCategory4.tsx
+++ src/components/cards/CardCategory4.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { TaxonomyType } from "@/data/types";
 import convertNumbThousand from "@/utils/convertNumbThousand";
 import Link from "next/link";
 import Image from "next/image";
+import SkeletonImage from "@/shared/SkeletonImage";
 
 export interface CardCategory4Props {
   className?: string;
@@ -13,6 +14,19 @@ const CardCategory4: FC<CardCategory4Props> = ({
   className = "",
   taxonomy,
 }) => {
+  const [loading, setLoading] = useState(true);
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    if (imageLoaded) {
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 3000); 
+
+      return () => clearTimeout(timer);
+    }
+  }, [imageLoaded]);
+
   const { count, name, href = "/", thumbnail, listingType } = taxonomy;
   return (
     <Link
@@ -22,33 +36,18 @@ const CardCategory4: FC<CardCategory4Props> = ({
     >
       <div
         className={`flex-shrink-0 relative w-full aspect-w-4 aspect-h-2 sm:aspect-h-2 h-0 rounded-2xl overflow-hidden group`}
-        //  style={{ width: '300px', height: '300px' }}
       >
+        {loading && <SkeletonImage className="absolute inset-0" />}{" "}
         <Image
           src={thumbnail || ""}
           className="object-cover w-full h-full rounded-2xl"
           fill
           alt="archive"
           sizes="(max-width: 400px) 100vw, 400px"
+          onLoadingComplete={() => setImageLoaded(true)}
         />
         <span className="opacity-0 group-hover:opacity-100 absolute inset-0 bg-black bg-opacity-10 transition-opacity"></span>
       </div>
-      {/* <div className="mt-4 px-2 truncate text-center">
-        <h2
-          className={`text-base sm:text-lg text-neutral-900 dark:text-neutral-100 font-medium truncate`}
-        >
-          {name}
-        </h2>
-        <span
-          className={`block mt-2 text-sm text-neutral-6000 dark:text-neutral-400`}
-        >
-          {convertNumbThousand(count || 0)}
-          {` `}
-          {(!listingType || listingType === "stay") && "properties"}
-          {listingType === "car" && "cars"}
-          {listingType === "experiences" && "experiences"}
-        </span>
-      </div> */}
     </Link>
   );
 };
