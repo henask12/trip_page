9a16df70a35ee9a49f8485ab655234ea27b41f97
Henok Mekonnen <Henokmekonnen5@gmail.com>
2024-08-03 23:48:03 +0300

    added some packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,14 +26,13 @@ module.exports = {
         "2xl": "128px",
       },
     },
-    // fontFamily: {
-    //   display: ["var(--font-display)", ...defaultTheme.fontFamily.sans],
-    //   body: ["var(--font-body)", ...defaultTheme.fontFamily.sans],
-    // },
 
     extend: {
       backgroundImage: {
         'custom-bg': "url('https://plus.unsplash.com/premium_photo-1661962660197-6c2430fb49a6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+      translate: {
+        '-50%': '-50%',
       },
       colors: {
        
