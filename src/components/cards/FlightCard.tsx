b66ac12e197c30c2fff148e75311c3c87737a877
Henok Mekonnen <Henokmekonnen5@gmail.com>
2024-08-06 19:22:23 +0300

    flight card updated

--- src/components/cards/FlightCard.tsx
+++ src/components/cards/FlightCard.tsx
@@ -5,30 +5,38 @@ import { FlightCardProps } from "../type";
 import FlightDetail from "./flight/FlightCardDetails";
 import FlightCardHeader from "./flight/FlightCardHeader";
 
-
-
-const FlightCard: FC<FlightCardProps> = ({ className = "", data, isSheetOpen, setIsSheetOpen, onCloseSheet }) => {
+const FlightCard: FC<FlightCardProps> = ({
+  className = "",
+  data,
+  isSheetOpen,
+  setIsSheetOpen,
+  onCloseSheet,
+  onSelect,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedFlightId, setSelectedFlightId] = useState<string | null>(null);
 
   const handleSelect = (id: string) => {
+    debugger
     setSelectedFlightId(id);
+    if (onSelect) {
+      onSelect(id);
+    }
   };
   const renderDetail = () => {
     if (!isOpen) return null;
     return (
       <div className="p- md:p-8 border border-neutral-200 dark:border-neutral-700 rounded-2xl">
-       
-        {<FlightDetail data={data}  onSelect={handleSelect} 
-          isSelected={selectedFlightId === data.id} isSheetOpen={isSheetOpen} setIsSheetOpen={setIsSheetOpen} onCloseSheet={onCloseSheet}/>}
-        <div className="my-7 md:my-10 space-y-5 md:pl-24">
-          <div className="border-t border-neutral-200 dark:border-neutral-700" />
-          <div className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base">
-            Transit time: {data.transitTime}
-          </div>
-          <div className="border-t border-neutral-200 dark:border-neutral-700" />
-        </div>
-        {/* {<FlightDetail data={data} />} */}
+        {
+          <FlightDetail
+            data={data}
+            onSelect={handleSelect}
+            isSelected={selectedFlightId === data.id}
+            isSheetOpen={isSheetOpen}
+            setIsSheetOpen={setIsSheetOpen}
+            onCloseSheet={onCloseSheet}
+          />
+        }
       </div>
     );
   };
