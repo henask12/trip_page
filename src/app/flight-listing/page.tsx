82066671f4a7f23eee49f7a01bc756d01e977fcc
Henok Mekonnen <Henokmekonnen5@gmail.com>
2024-08-06 19:21:49 +0300

    page tsx file

--- src/app/flight-listing/page.tsx
+++ src/app/flight-listing/page.tsx
@@ -13,7 +13,11 @@ import { useDispatch, useSelector } from "react-redux";
 import FlightInfo from "@/components/cards/flight/FlightInfo";
 import useScrollHandler from "@/utils/useScroll";
 import { setFlightSearchState } from "@/features/flightSearch/flightSearchSlice";
-import { roundTripBusiness } from "@/data/searchData";
+import {
+  FlightSection,
+  multiCityEconomy,
+  roundTripBusiness,
+} from "@/data/searchData";
 
 const dateRanges = [
   { range: "Aug 06 – Aug 20", price: "View" },
@@ -41,8 +45,8 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     transitTime: "04:00",
     duration: "06:00",
     flightNumber: "KE0654",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "LAX",
+    destination: "ADD",
     airlines: {
       logo: "https://www.gstatic.com/flights/airline_logos/70px/KE.png",
       name: "Korean Air",
@@ -60,8 +64,8 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     departureTime: "10:00",
     stops: "1 Stop",
     arrivalTime: "20:00",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "LAX",
+    destination: "ADD",
     duration: "06:00",
     transitTime: "04:00",
     flightNumber: "KE0654",
@@ -81,8 +85,8 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     price: "$2,380",
     departureTime: "10:00",
     flightNumber: "KE0654",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "ADD",
+    destination: "LAX",
     duration: "06:00",
     stops: "1 Stop",
     arrivalTime: "20:00",
@@ -99,7 +103,7 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     },
   },
   {
-    id: "1",
+    id: "4",
     price: "$4,100",
     departureTime: "10:00",
     flightNumber: "KE0654",
@@ -107,8 +111,8 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     transitTime: "04:00",
     stops: "1 Stop",
     arrivalTime: "20:00",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "ADD",
+    destination: "LAX",
     airlines: {
       logo: "https://www.gstatic.com/flights/airline_logos/70px/KE.png",
       name: "Korean Air",
@@ -121,7 +125,7 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     },
   },
   {
-    id: "2",
+    id: "5",
     price: "$3,380",
     departureTime: "10:00",
     flightNumber: "KE0654",
@@ -129,8 +133,8 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     transitTime: "04:00",
     stops: "1 Stop",
     arrivalTime: "20:00",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "LAX",
+    destination: "ADD",
     airlines: {
       logo: "https://www.gstatic.com/flights/airline_logos/70px/SQ.png",
       name: "Singapore Airlines",
@@ -143,14 +147,14 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     },
   },
   {
-    id: "1",
+    id: "6",
     price: "$4,100",
     departureTime: "10:00",
     flightNumber: "KE0654",
     duration: "06:00",
     transitTime: "04:00",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "ADD",
+    destination: "LAX",
     stops: "1 Stop",
     arrivalTime: "20:00",
     airlines: {
@@ -165,12 +169,12 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     },
   },
   {
-    id: "2",
+    id: "7",
     price: "$3,380",
     departureTime: "10:00",
     flightNumber: "KE0654",
-    origin: "ADD (Addis Ababa Airport)",
-    destination: "DXB (Dubai International Airport)",
+    origin: "LAX",
+    destination: "ADD",
     transitTime: "04:00",
     duration: "06:00",
     stops: "1 Stop",
@@ -187,41 +191,44 @@ const DEMO_DATA: FlightCardProps["data"][] = [
     },
   },
 ];
+
+interface SelectedFlight {
+  departingFlight?: string;
+  returningFlight?: string;
+  multiCityFlights?: string[];
+}
 const FlightListingPage: FC = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setFlightSearchState(roundTripBusiness));
+    dispatch(setFlightSearchState(multiCityEconomy));
   }, [dispatch]);
-
 
   const { flightSections } = useSelector(
     (state: RootState) => state.flightSearch
   );
 
- useEffect(() => {
+  useEffect(() => {
     console.log(flightSections);
   }, [flightSections]);
 
+  const [selectedFlights, setSelectedFlights] = useState<SelectedFlight>({});
+  const [currentSearchState, setCurrentSearchState] = useState<string | null>(
+    null
+  );
+  const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
+
   const [filter, setFilter] = useState<string | null>(null);
-  const [isSheetOpen, setIsSheetOpen] = useState(false);
-
-  const filteredData = DEMO_DATA.filter((stay) => {
-    if (!filter) return true;
-
-  }).slice(0, 10); 
 
   const [showFlightInfo, setShowFlightInfo] = useState<boolean>(false);
 
-  const handleSetShowFlightInfo = useCallback<React.Dispatch<React.SetStateAction<boolean>>>(
-    (newState) => {
-      setShowFlightInfo(newState);
-    },
-    []
-  );
+  const handleSetShowFlightInfo = useCallback<
+    React.Dispatch<React.SetStateAction<boolean>>
+  >((newState) => {
+    setShowFlightInfo(newState);
+  }, []);
 
   useScrollHandler({ setShowFlightInfo: handleSetShowFlightInfo });
-
 
   const renderSidebar = () => {
     return (
@@ -244,21 +251,126 @@ const FlightListingPage: FC = () => {
   const handleSheetClose = () => {
     setOpenSheetId(null);
   };
-  const renderSection1 = () => {
-    return (
-      <div className="listingSection__wrap mt-48">
-        <Categories onFilterChange={setFilter} />
-        {DEMO_DATA.filter((_, i) => i < 10).map((stay) => (
-          <FlightCard
-            key={stay.id}
-            data={stay}
-            isSheetOpen={openSheetId === stay.id}
-            setIsSheetOpen={() => handleSheetOpen(stay.id)}
-            onCloseSheet={handleSheetClose}
-          />
-        ))}
-      </div>
-    );
+
+  // useEffect(() => {
+  //   if (selectedFlights.departingFlight) {
+  //     setIsDepartingSelected(true);
+  //   } else {
+  //     setIsDepartingSelected(false);
+  //   }
+  // }, [selectedFlights.departingFlight]);
+
+  useEffect(() => {
+    setCurrentSearchState(`Departing To ADD`);
+
+    if (selectedFlights.departingFlight) {
+      setCurrentSearchState(`Returning To LAX`);
+    }
+  }, [selectedFlights, currentSearchState]);
+
+  const renderFlights = (
+    tripType: string,
+    flights: FlightCardProps["data"][]
+  ) => {
+    switch (tripType) {
+      case "One-Way":
+        return flights.map((flight) => {
+          return (
+            <FlightCard
+              key={flight.id}
+              data={flight}
+              // onSelect={() => {setSelectedFlights({ departingFlight: flight.id setCurrentSearchState(`Departing to ${flight?.destination}`);})}}
+              onSelect={() => {
+                setSelectedFlights({ departingFlight: flight.id });
+                setCurrentSearchState(`Departing to ${flight?.destination}`);
+              }}
+              isSheetOpen={openSheetId === flight.id}
+              setIsSheetOpen={() => handleSheetOpen(flight.id)}
+              onCloseSheet={handleSheetClose}
+            />
+          );
+        });
+
+      case "Round-Trip":
+        if (!selectedFlights.departingFlight) {
+          return flights
+            .filter(
+              (flight) =>
+                flight.origin === "LAX" && flight.destination === "ADD"
+            ) // Departing flight criteria
+            .map((flight) => {
+              return (
+                <FlightCard
+                  key={flight.id}
+                  data={flight}
+                  onSelect={() =>
+                    setSelectedFlights({ departingFlight: flight.id })
+                  }
+                  isSheetOpen={openSheetId === flight.id}
+                  setIsSheetOpen={() => handleSheetOpen(flight.id)}
+                  onCloseSheet={handleSheetClose}
+                />
+              );
+            });
+        } else {
+          return flights
+            .filter(
+              (flight) =>
+                flight.origin === "ADD" && flight.destination === "LAX"
+            ) // Returning flight criteria
+            .map((flight) => {
+              return (
+                <FlightCard
+                  key={flight.id}
+                  data={flight}
+                  isSheetOpen={openSheetId === flight.id}
+                  setIsSheetOpen={() => handleSheetOpen(flight.id)}
+                  onCloseSheet={handleSheetClose}
+                  onSelect={() =>
+                    setSelectedFlights((prevState) => ({
+                      ...prevState,
+                      returningFlight: flight.id,
+                    }))
+                  }
+                />
+              );
+            });
+        }
+
+      case "Multi-City":
+        const section = flightSections[currentSectionIndex];
+        const filteredFlights = flights.filter(
+          (flight) =>
+            flight.origin === section.origin &&
+            flight.destination === section.destination
+        );
+        return filteredFlights.map((flight) => {
+          return (
+            <FlightCard
+              key={flight.id}
+              data={flight}
+              isSheetOpen={openSheetId === flight.id}
+              setIsSheetOpen={() => handleSheetOpen(flight.id)}
+              onCloseSheet={handleSheetClose}
+              onSelect={() => {
+                setSelectedFlights((prevState) => ({
+                  ...prevState,
+                  multiCityFlights: [
+                    ...(prevState.multiCityFlights || []),
+                    flight.id,
+                  ],
+                }));
+                if (currentSectionIndex < flightSections.length - 1) {
+                  setCurrentSectionIndex((prevIndex) => prevIndex + 1);
+                }
+              }}
+            />
+          );
+        });
+
+      default:
+        return null;
+    }
   };
 
   return (
@@ -272,27 +384,36 @@ const FlightListingPage: FC = () => {
       </div>
 
       {!openSheetId && showFlightInfo && (
-        <div className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md rounded-md">
+        <div className="fixed top-0 left-0 right-0 z-50 bg-white container shadow-md rounded-md">
           <FlightInfo
             departure="New York"
             destination="Los Angeles"
             passengerCount={1}
             cabinClass="Economy"
             travelDates="2024-08-01 to 2024-08-15"
+            dateRanges={dateRanges}
           />
         </div>
       )}
       <div className="mx-auto px-4 mt-6 relative mb-24 lg:mb-28 inset-0 bg-gray-100 dark:bg-black/20  inset-y-0 w-screen  rounded-xl xl:rounded-tl-[40px] xl:rounded-tr-[40px] z-0 ">
         <div className={`nc-AuthorPage `}>
           <main className="container mt-12 mb-24 lg:mb-32 flex flex-col lg:flex-row">
-            <div className="hidden md:flex absolute items-center justify-center mt-20 ml-6 mx-auto bg-white px-4 sm:px-2 md:px-4 lg:px-4 rounded-lg dark:bg-black/20 z-30">
-              <DateTabs dateRanges={dateRanges} />
-            </div>
+            {!showFlightInfo && (
+              <div className="hidden md:flex absolute items-center justify-center mt-20 ml-6 mx-auto bg-white px-4 sm:px-2 md:px-4 lg:px-4 rounded-lg dark:bg-black/20 z-30">
+                <DateTabs dateRanges={dateRanges} />
+              </div>
+            )}
             <div className="block flex-grow mb-24 lg:mb-0">
               <div className="lg:sticky lg:top-24">{renderSidebar()}</div>
             </div>
             <div className="w-full lg:w-4/5 xl:w-2/3 space-y-4 lg:space-y-6 lg:pl-4 flex-shrink-0">
-              {renderSection1()}
+              <div className="listingSection__wrap mt-48">
+                <Categories
+                  status={currentSearchState}
+                  onFilterChange={setFilter}
+                />
+                {renderFlights("Round-Trip", DEMO_DATA)}
+              </div>
             </div>
           </main>
         </div>
